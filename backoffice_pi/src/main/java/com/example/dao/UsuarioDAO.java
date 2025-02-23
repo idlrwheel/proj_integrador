d0a149f4d0147911ed2a6d64abdba7e72725ed8a
Arthur Hyppolito <38328154+Arthur-Hyppolito@users.noreply.github.com>
2025-02-23 04:01:45 -0300

    Update UsuarioDAO.java

--- backoffice_pi/src/main/java/com/example/dao/UsuarioDAO.java
+++ backoffice_pi/src/main/java/com/example/dao/UsuarioDAO.java
@@ -2,21 +2,18 @@ package com.example.dao;
 
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 import com.example.models.Usuario;
 
 public class UsuarioDAO {
     private static final String DB_URL = "jdbc:mysql://localhost:3306/ecommerce_pi";
-    private static final String DB_username = "root";
-    private static final String DB_password = "Password0108!";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "";
 
     public Usuario validarLogin(String email, String senha) {
-        try (Connection connection = DriverManager.getConnection(DB_URL, DB_username, DB_password)) {
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
             System.out.println("Conexão estabelecida com o banco de dados.");
 
             String sql = "SELECT * FROM userBackoffice WHERE email = ?";
@@ -28,6 +25,7 @@ public class UsuarioDAO {
                 String senhaHash = rs.getString("senha");
                 String grupo = rs.getString("tipoUser");
                 String status = rs.getString("status");
+                String nome = rs.getString("nome");
 
                 if (!status.equals("ativado") || (!grupo.equals("adm") && !grupo.equals("estoquista"))) {
                     System.out.println("Acesso negado! Status ou grupo inválido.");
@@ -36,7 +34,7 @@ public class UsuarioDAO {
 
                 if (encriptarSenha(senha).equals(senhaHash)) {
                     System.out.println("Senhas coincidem. Login bem-sucedido.");
-                    return new Usuario(email, grupo, status);
+                    return new Usuario(email, grupo, status, nome);
                 } else {
                     System.out.println("Senha incorreta!");
                 }
@@ -61,6 +59,25 @@ public class UsuarioDAO {
         return sb.toString();
     }
 
+    // Novo método para listar todos os usuários
+    public static List<Usuario> listarUsuarios() {
+        List<Usuario> usuarios = new ArrayList<>();
+        try (Connection connection = DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD)) {
+            String sql = "SELECT * FROM userBackoffice";
+            PreparedStatement statement = connection.prepareStatement(sql);
+            ResultSet rs = statement.executeQuery();
+
+            while (rs.next()) {
+                String email = rs.getString("email");
+                String grupo = rs.getString("tipoUser");
+                String status = rs.getString("status");
+                String nome = rs.getString("nome");
+                // Adicionando o usuário à lista
+                usuarios.add(new Usuario(email, grupo, status, nome));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return usuarios;
+    }
 }
-
-
