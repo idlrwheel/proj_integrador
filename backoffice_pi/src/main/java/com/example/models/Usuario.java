84964c35cff5848b6a561fab5e1600c40ec5bc5b
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2025-02-24 00:23:46 -0300

    Update Usuario.java

--- backoffice_pi/src/main/java/com/example/models/Usuario.java
+++ backoffice_pi/src/main/java/com/example/models/Usuario.java
@@ -1,11 +1,16 @@
 package com.example.models;
 
 public class Usuario {
+    private int id;
+    private static int valor = 0;
     private String email;
     private String grupo;
     private String status;
     private String nome; // Nome foi adicionado ao modelo para poder ser usado
     private String cpf;
+
+    public Usuario(){
+    }
 
     public Usuario(String email, String grupo, String status, String nome, String cpf) {
         this.email = email;
@@ -13,8 +18,12 @@ public class Usuario {
         this.status = status;
         this.nome = nome; // Inicializando o nome
         this.cpf = cpf;
+        id = valor++;
     }
-
+    
+    public int getId(){
+        return id;
+    }
     public String getEmail() {
         return email;
     }
