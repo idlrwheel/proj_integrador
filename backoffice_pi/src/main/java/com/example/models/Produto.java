a52cf2c22eafe72aa34c9a07be9c31db9a86f25b
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2025-03-09 16:18:47 -0300

    Update Produto.java

--- backoffice_pi/src/main/java/com/example/models/Produto.java
+++ backoffice_pi/src/main/java/com/example/models/Produto.java
@@ -3,45 +3,89 @@ package com.example.models;
 public class Produto {
     private int codigo;
     private String nome;
-    private String descricao;
-    private double valorProduto;
+    private double avaliacao;
+    private String descricaoDetalhada;
     private int qtdEstoque;
-    private static int i = 0;
+    private double valorProduto;
+    private String status;
 
-    public Produto(String nome, String descricao, double valorProduto, int qtdEstoque) {
+    public Produto(int codigo, String nome, double avaliacao, String descricaoDetalhada, int qtdEstoque, double valorProduto, String status) {
+        this.codigo = codigo;
         this.nome = nome;
-        this.descricao = descricao;
-        this.valorProduto = valorProduto;
+        this.avaliacao = avaliacao;
+        this.descricaoDetalhada = descricaoDetalhada;
         this.qtdEstoque = qtdEstoque;
-        codigo = i++;
+        this.valorProduto = valorProduto;
+        this.status = status;
     }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
     public void setCodigo(int codigo) {
         this.codigo = codigo;
     }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
     public String getNome() {
         return nome;
     }
+
+    
+
     public void setNome(String nome) {
         this.nome = nome;
     }
+
     public String getDescricao() {
-        return descricao;
+        return descricaoDetalhada;
     }
+
     public void setDescricao(String descricao) {
-        this.descricao = descricao;
+        this.descricaoDetalhada = descricao;
     }
+
     public double getValorProduto() {
         return valorProduto;
     }
+
     public void setValorProduto(double valorProduto) {
         this.valorProduto = valorProduto;
     }
+
     public int getQtdEstoque() {
         return qtdEstoque;
     }
+
     public void setQtdEstoque(int qtdEstoque) {
         this.qtdEstoque = qtdEstoque;
     }
+
+    public double getAvaliacao() {
+        return avaliacao;
+    }
+
+    public void setAvaliacao(double avaliacao) {
+        this.avaliacao = avaliacao;
+    }
+
+    public String getDescricaoDetalhada() {
+        return descricaoDetalhada;
+    }
+
+    public void setDescricaoDetalhada(String descricaoDetalhada) {
+        this.descricaoDetalhada = descricaoDetalhada;
+    }
+    
+}
 
     
 }
