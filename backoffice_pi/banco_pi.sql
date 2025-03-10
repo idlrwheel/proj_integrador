0e39da9bcd69bc6cf5ab8e2ed9bc1af1153ea4a7
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2025-03-10 16:39:28 -0300

    Update banco_pi.sql

--- backoffice_pi/banco_pi.sql
+++ backoffice_pi/banco_pi.sql
@@ -28,7 +28,7 @@ INSERT INTO userBackoffice (email, senha, tipoUser, status) VALUES
 ('cliente@teste.com', 'senha321', 'cliente', 'ativado');
 UPDATE userBackoffice SET senha = '2288821c6b799cf47a8c9aa231f361ffb906bbee0d5fb5e1767509e27442cc62' WHERE email = 'cliente@teste.com';
 
-CREATE TABLE produtos(
+CREATE TABLE IF NOT EXISTS produtos(
 codigo INT auto_increment PRIMARY KEY,
 nome varchar(200) NOT NULL,
 avaliacao DECIMAL(2,1) CHECK (avaliacao >= 1 AND avaliacao <= 5),
@@ -36,14 +36,44 @@ descricaoDetalhada varchar(2000) NOT NULL,
 qtdEstoque int NOT NULL,
 valorProduto DECIMAL(10,2) NOT NULL);
 
-CREATE TABLE imagensProduto (
+CREATE TABLE IF NOT EXISTS imagensProduto (
     id INT AUTO_INCREMENT PRIMARY KEY,
     produto_id INT NOT NULL,
     nome_arquivo VARCHAR(255) NOT NULL,
     diretorio_origem VARCHAR(255) NOT NULL,
-    principal BOOLEAN NOT NULL,
+    principal BOOLEAN NOT NULL DEFAULT FALSE,
     FOREIGN KEY (produto_id) REFERENCES produtos(codigo) ON DELETE CASCADE
 );
+
+CREATE TRIGGER before_insert_imagem
+BEFORE INSERT ON imagensProduto
+FOR EACH ROW
+BEGIN
+    IF NEW.principal = TRUE THEN
+        UPDATE imagensProduto SET principal = FALSE WHERE produto_id = NEW.produto_id;
+    END IF;
+END;
+
+CREATE PROCEDURE alterarImagemProduto(
+    IN p_id_imagem INT,
+    IN p_novo_nome VARCHAR(255),
+    IN p_novo_diretorio VARCHAR(255),
+    IN p_principal BOOLEAN
+)
+
+BEGIN
+    IF p_principal = TRUE THEN
+        UPDATE imagensProduto 
+        SET principal = FALSE 
+        WHERE produto_id = (SELECT produto_id FROM imagensProduto WHERE id = p_id_imagem);
+    END IF;
+
+    UPDATE imagensProduto 
+    SET nome_arquivo = p_novo_nome, 
+        diretorio_origem = p_novo_diretorio, 
+        principal = p_principal 
+    WHERE id = p_id_imagem;
+END;
 
 /*Adicionar a coluna de Status no banco de dados.  */
 ALTER TABLE produtos ADD COLUMN status ENUM('ativo', 'desativado') NOT NULL DEFAULT 'ativo';
