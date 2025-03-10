b446c85de132dc863671882adaff53ac32184ae1
Vitorwgs <167483937+Vitorwgs@users.noreply.github.com>
2025-03-10 17:19:39 -0300

    Update ImagemProduto.java

--- backoffice_pi/src/main/java/com/example/models/ImagemProduto.java
+++ backoffice_pi/src/main/java/com/example/models/ImagemProduto.java
@@ -2,12 +2,16 @@ package com.example.models;
 
 public class ImagemProduto {
     private int id;
+    private int produtoId;
     private String nomeArquivo;
+    private String diretorioOrigem;
     private boolean principal;
 
-    public ImagemProduto(int id, String nomeArquivo, boolean principal) {
+    public ImagemProduto(int id, int produtoId, String nomeArquivo, String diretorioOrigem, boolean principal) {
         this.id = id;
+        this.produtoId = produtoId;
         this.nomeArquivo = nomeArquivo;
+        this.diretorioOrigem = diretorioOrigem;
         this.principal = principal;
     }
 
@@ -19,12 +23,28 @@ public class ImagemProduto {
         this.id = id;
     }
 
+    public int getProdutoId() {
+        return produtoId;
+    }
+
+    public void setProdutoId(int produtoId) {
+        this.produtoId = produtoId;
+    }
+
     public String getNomeArquivo() {
         return nomeArquivo;
     }
 
     public void setNomeArquivo(String nomeArquivo) {
         this.nomeArquivo = nomeArquivo;
+    }
+
+    public String getDiretorioOrigem() {
+        return diretorioOrigem;
+    }
+
+    public void setDiretorioOrigem(String diretorioOrigem) {
+        this.diretorioOrigem = diretorioOrigem;
     }
 
     public boolean isPrincipal() {
