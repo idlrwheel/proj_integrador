8b1ff4709e2cecf0de5c00eac2d1142744e1acc5
giovanna ramos <161260067+idlrwheel@users.noreply.github.com>
2025-03-31 16:25:15 -0300

    correção do ProdutoRepository.java

--- ecommerce/backend/src/main/java/com/example/ecommerce/repository/ProdutoRepository.java
+++ ecommerce/backend/src/main/java/com/example/ecommerce/repository/ProdutoRepository.java
@@ -1,21 +1,11 @@
-import com.example.ecommerce.model.Produto;
-import com.example.ecommerce.repository.ProdutoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-
-@RestController
-@RequestMapping("/produtos")
-public class ProdutoRepository{
-    @Autowired
-    private ProdutoRepository produtoRepository;
-
-    @GetMapping
-    public List<Produto> listarProdutos() {
-        return produtoRepository.findAll();
-    }
+package com.example.ecommerce.repository;
+ 
+ import com.example.ecommerce.model.Produto;
+ import org.springframework.data.jpa.repository.JpaRepository;
+ import org.springframework.stereotype.Repository;
+ 
+ @Repository
+ public interface ProdutoRepository extends JpaRepository<Produto, Integer> {}
 }
 
 
